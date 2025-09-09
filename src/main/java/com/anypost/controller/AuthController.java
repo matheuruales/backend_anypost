1cb7ecedda1b819ba3db55d3b62ebd743d648623
Matheu2b2t <johnatan.ruales@campusucc.edu.co>
2025-09-09 18:50:36 -0500

    chore: trigger build

--- src/main/java/com/anypost/controller/AuthController.java
+++ src/main/java/com/anypost/controller/AuthController.java
@@ -1,16 +1,23 @@
 package com.anypost.controller;
 
-import com.anypost.dto.AuthResponse;
-import com.anypost.dto.LoginRequest;
-import com.anypost.service.FirebaseService;
-import com.anypost.service.UserProfileService;
-import com.anypost.model.UserProfile;
-import com.google.firebase.auth.FirebaseToken;
-import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anypost.dto.AuthResponse;
+import com.anypost.dto.LoginRequest;
+import com.anypost.model.UserProfile;
+import com.anypost.service.FirebaseService;
+import com.anypost.service.UserProfileService;
+import com.google.firebase.auth.FirebaseToken;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -90,3 +97,4 @@ public class AuthController {
         }
     }
 }
+//xd
